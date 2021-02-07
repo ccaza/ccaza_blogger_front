1ced6cddf6f86e8045a99cc98bff21a3405dde0c
ccaza <support@ccaza.com>
2021-02-07 19:35:55 +0800

    Change webpack dev port

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,31 @@
-const React = require("react");
 import { BrowserRouter as Router } from "react-router-dom";
 import Header from "../components/Header.jsx";
+const axios = require("axios").default;
+import React, { useState, useEffect } from "react";
 
+function xx(setCount) {
+  axios
+    .get("http://127.0.0.1:8080/")
+    .then(function (response) {
+      setCount(response.data);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    });
+}
 export default function Home() {
+  const [count, setCount] = useState("");
+  useEffect(() => {
+    if (count === "") {
+      xx(setCount);
+    }
+  });
   return (
     <Router>
       <div>
         <Header />
-        <h2>这是Home页面</h2>
+        <h2>这是Home页面 {count} </h2>
       </div>
     </Router>
   );
